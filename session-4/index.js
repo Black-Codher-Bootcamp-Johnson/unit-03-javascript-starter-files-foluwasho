a55c06cacb6cbf7e8e5992f13d34770b56259d6e
Debbie Adewodu <debbieadewodu@Debbies-MacBook-Air.local>
2022-01-24 21:39:32 +0000

    statements in javascript

--- session-4/index.js
+++ session-4/index.js
@@ -1,8 +1,42 @@
-// ****************************************************TASK 1 - OBJECTS****************************************************
 
+let personA = {
+  name: 'Anna' ,
+  age: '23' ,
+  location: 'paris' ,
+  likes: 'pizza' ,
 
+}
 
+let personB = {
+    name: 'Maria' ,
+    age: '28' ,
+    location: 'Portugal' ,
+    likes: 'Ice-cream' ,
+  
+  }
 
+  let personC = {
+    name: 'Valerie' ,
+    age: '30' ,
+    location: 'Canada' ,
+    likes: 'Travelling' ,
+
+  }
+  
+  let personD = {
+    name: 'Ayo' ,
+    age: '36' ,
+    location: 'Nigeria' ,
+    likes: 'Baking' ,
+  }
+    function biography(person) {
+    console.log(' Hi, my name is ' + person.name + ' and I am ' + person.age + ' years old, I live in' 
+    + person.location + ' and I like' + person.likes)  
+    }
+    biography(personA);
+    biography(personB);
+    biography(personC);
+    biography(personD);
 
 
 
@@ -19,25 +53,27 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
 
+const addition3 = x + y + y
+console.log("Addition3: x + y + y " + addition3);
 
 
 
